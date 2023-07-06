eadf18def2f41122130b96ab1abb41cceb4c0ce4
Kanwal <akanwal@informatica.com>
2023-07-06 14:43:24 +0530

    Updated

--- src/main/java/org/example/SCP.java
+++ src/main/java/org/example/SCP.java
@@ -1,9 +1,8 @@
 package org.example;
 
-import com.jcraft.jsch.Channel;
-import com.jcraft.jsch.ChannelSftp;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.Session;
+import com.jcraft.jsch.*;
+
+import java.io.InputStream;
 
 public class SCP {
     private static final String hostname = "invarjun0201";
@@ -20,6 +19,8 @@ public class SCP {
 
         // Linux to Local Windows
 //        operationType("get");
+        String res = executeCommand("ls /root/Documents");
+        System.out.println(res);
     }
 
     private static void operationType(String op) {
@@ -45,4 +46,36 @@ public class SCP {
             e.printStackTrace();
         }
     }
+
+    private static String executeCommand(String command) {
+        StringBuilder outputBuffer = new StringBuilder();
+
+        try {
+            JSch jsch = new JSch();
+            Session session = jsch.getSession(username, hostname, 22);
+            session.setPassword(password);
+            session.setConfig("StrictHostKeyChecking", "no");
+            session.connect();
+            Channel channel = session.openChannel("exec");
+            ((ChannelExec)channel).setCommand(command);
+
+            channel.connect();
+            InputStream commandOutput = channel.getInputStream();
+            int readByte = commandOutput.read();
+
+            while(readByte != 0xffffffff)
+            {
+                outputBuffer.append((char)readByte);
+                readByte = commandOutput.read();
+            }
+            channel.disconnect();
+            session.disconnect();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return outputBuffer.toString();
+    }
+
+
 }
